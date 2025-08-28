4bc035556067722e4f854fb36db21a6304b68b73
Ariel Messing <ariel.messing@gmail.com>
2025-08-28 19:12:47 +0100

    Add deprecated fields - for Jackson

--- src/main/java/arielmessing/ebird/api/hotspots/HotspotInfo.java
+++ src/main/java/arielmessing/ebird/api/hotspots/HotspotInfo.java
@@ -1,13 +1,10 @@
 package arielmessing.ebird.api.hotspots;
 
-import com.fasterxml.jackson.annotation.JsonAlias;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public record HotspotInfo(
         String locId,
-        @JsonAlias("locName") String name,
-        @JsonAlias("lat") double latitude,
-        @JsonAlias("lng") double longitude,
+        String name,
+        double latitude,
+        double longitude,
         String countryCode,
         String countryName,
         String subnational1Name,
@@ -16,4 +13,7 @@ public record HotspotInfo(
         String subnational2Code,
         boolean isHotspot,
         String hierarchicalName,
-        @Deprecated String locID) {}
+        @Deprecated String locID,
+        @Deprecated String locName,
+        @Deprecated double lat,
+        @Deprecated double lng) {}
